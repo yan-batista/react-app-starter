857399d3dbd6983f6fe2959491f6254742e74809
yan-batista <ybatistasouza@hotmail.com>
2024-09-11 01:37:28 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-emerald-500">
+      Hello World
+    </h1>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+export default {
+  content: ['./src/**/*.tsx', './index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
